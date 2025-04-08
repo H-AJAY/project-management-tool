451eed6088a6e001068d73a50883b144ef44b3fd
AjayHochumin <ajay.hochumin@lethalbrains.in>
2025-04-08 16:15:01 +0530

    Latest changes

--- project-management-tool-backend/server.js
+++ project-management-tool-backend/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
+console.log("✅ MONGO_URI loaded:", process.env.MONGO_URI);
 
 const projectRoutes = require("./routes/projectRoutes");
 const taskRoutes = require("./routes/taskRoutes");
